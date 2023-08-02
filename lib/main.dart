bae18c80fc92b043cf2f7ac79c461d9fd91c450d
huchujj <tjddk6662@naver.com>
2023-08-02 16:01:52 +0900

    refactor : ListCard 위젯 분리
    이전에 main.dart애 있던 Container 위젯을 widgets/list_card.dart로
    분리시킴
    클래스명은 ListCard
    
    issues : #1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ojakgyo/widgets/list_card.dart';
 
 void main() {
   runApp(const App());
@@ -23,53 +24,11 @@ class _AppState extends State<App> {
               horizontal: 25,
             ),
             child: Column(
-              children: [
-                const SizedBox(
+              children: const [
+                SizedBox(
                   height: 60,
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF1F2123),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(30),
-                    child: Column(
-                      children: const [
-                        Text(
-                          '거래 상태',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          '거래 날짜',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          '거래자',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          '거래 상품',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          '거래 가격',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                ListCard(),
               ],
             ),
           ),
