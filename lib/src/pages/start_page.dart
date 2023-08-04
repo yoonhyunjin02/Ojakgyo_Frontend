ce25ccf00ea6cda207882c124bdf0ca33a18166e
yoonhyunjin02 <hyeonjin-hyunjin@naver.com>
2023-08-05 05:44:00 +0900

    Feat : 시작 페이지 생성
    시작 페이지를 생성하였습니다.
    
    issues : #2

---
+++ lib/src/pages/start_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({super.key});
+
+  @override
+  State<StartPage> createState() => _AppState();
+}
+
+class _AppState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
