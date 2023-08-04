68e6884468e533c8f4de471155837ec4a40850e5
huchujj <tjddk6662@naver.com>
2023-08-04 20:27:35 +0900

    Feat : 마이페이지에 프로필 창 추가
    마이페이지에 프로필을 한 눈에 볼 수 있는 창을 생성
    클릭시 어떠한 기능도 없음
    
    issues : #18

--- lib/src/pages/my_page.dart
+++ lib/src/pages/my_page.dart
@@ -19,13 +19,52 @@ class _AppState extends State<MyPage> {
         child: Column(
           children: [
             Container(
-              decoration: const BoxDecoration(
-                color: Color(0xFF23225C),
+              decoration: BoxDecoration(
+                color: const Color(0xFFD8E8E9),
+                borderRadius: BorderRadius.circular(20),
               ),
-              child: const Text(
-                '마이페이지임',
-                style: TextStyle(color: Colors.white),
+              child: Padding(
+                padding: const EdgeInsets.all(26),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/profile1_img.png',
+                      height: 90,
+                      width: 90,
+                    ),
+                    const SizedBox(
+                      width: 18,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          '윤현진',
+                          style: TextStyle(
+                            color: Color.fromARGB(221, 21, 21, 21),
+                            fontSize: 23,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 3,
+                        ),
+                        Text(
+                          '010-1234-5678',
+                          style: TextStyle(
+                            color: Color.fromARGB(221, 53, 53, 53),
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
+            ),
+            const SizedBox(
+              height: 25,
             ),
           ],
         ),
