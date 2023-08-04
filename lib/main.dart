892306b7b9428d6461483803b09e43b749190c12
huchujj <tjddk6662@naver.com>
2023-08-04 17:08:02 +0900

    Feat : my_page.dart 파일 추가
    메인 페이지의 프로필 창을 클릭했을 때, 이동할 마이페이지 파일 생성
    경로는 src/pages/my_page/dart
    
    issues : #11

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:ojakgyo/src/pages/main_page.dart';
+import 'package:ojakgyo/src/pages/my_page.dart';
+
+//MainPage(userName: '윤현진',),
 
 void main() {
   runApp(const App());
@@ -16,9 +18,7 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MainPage(
-        userName: '윤현진',
-      ),
+      home: MyPage(),
     );
   }
 }
---
+++ lib/src/pages/my_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyPage extends StatefulWidget {
+  const MyPage({super.key});
+
+  @override
+  State<MyPage> createState() => _AppState();
+}
+
+class _AppState extends State<MyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(26),
+        child: Column(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: Color(0xFF23225C),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
